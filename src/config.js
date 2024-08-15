f193a497b636b995a531a9f1cac08c6674659f41
8uraj <surajyelpale7@gmail.com>
2024-08-16 00:04:14 +0530

    GoFLix 1.1 (Config Files added)

--- src/config.js
+++ src/config.js
@@ -1,20 +1,19 @@
-// replace below firebase config with your own
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCEy6YUeAvjOmRARvTRdfGeL8RJDzs8U7A",
-    authDomain: "netflix-clone-d1fa8.firebaseapp.com",
-    projectId: "netflix-clone-d1fa8",
-    storageBucket: "netflix-clone-d1fa8.appspot.com",
-    messagingSenderId: "1080602120235",
-    appId: "1:1080602120235:web:29e7c05415df6c1cd65a6e"
-  };
+  apiKey: "AIzaSyDxL99XNO4Fls8pAWZHcJM-gReyM6QLSrs",
+  authDomain: "gofliix-b07c9.firebaseapp.com",
+  projectId: "gofliix-b07c9",
+  storageBucket: "gofliix-b07c9.appspot.com",
+  messagingSenderId: "1003306223126",
+  appId: "1:1003306223126:web:d9cdf0b09d4ec3d2365737",
+  measurementId: "G-2MN2XPN5LW"
+};
 
 
 
-// craete a/c on themoviedb.org and replace access token below
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYWYyNDkwMzgyYTAxMDEzOTI3YzI0MzI3M2ZiZGY1MSIsIm5iZiI6MTcyMzc0NTM5OS4wMDA2NTEsInN1YiI6IjY2YmU0MmVhNTk2YzBiZmIwMzliODQ2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HubRmZNiCD32gAlN9p4DDtgWTC5VAeUjw7XKFc1V8DE";
 
 
-const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTNlNzczZDIwY2M2Y2NhNWQ4YWVjMjQzNTdlNDc1ZCIsInN1YiI6IjY2MjIyMDFhYWUzODQzMDE4NzJhNTJjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vPDW2QwNr9hIRdOvJx_x8hbHnDYZGHMtnZwfkqb3J8U";
+  export { TMDB_Access_Key, firebaseConfig };
 
-
-  export {firebaseConfig, TMDB_Access_Key};
